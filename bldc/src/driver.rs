8a2d64ae941a96235b904ba66fb49f576d9c0ab3
Tim Blakely <tim.blakely@gmail.com>
2021-04-02 15:32:48 -0700

    Starting up the FDCAN peripheral in the controller

--- bldc/src/driver.rs
+++ bldc/src/driver.rs
@@ -5,7 +5,7 @@ use crate::util::stm32::{clock_setup, clocks::G4_CLOCK_SETUP, disable_dead_batte
 
 pub struct Controller<S> {
     #[allow(dead_code)]
-    rcc: device::RCC,
+    fdcan: device::FDCAN1,
 
     #[allow(dead_code)]
     mode_state: S,
@@ -27,6 +27,7 @@ fn init(
     gpioa: device::GPIOA,
     gpiob: device::GPIOB,
     gpioc: device::GPIOC,
+    fdcan: device::FDCAN1,
 ) -> Controller<Init> {
     disable_dead_battery_pd(&pwr);
 
@@ -38,7 +39,12 @@ fn init(
         .acr
         .modify(|_, w| w.dcen().enabled().icen().enabled().prften().enabled());
 
-    // Enable the FDCAN clock
+    // FDCAN configuration
+    // Turn on PLLQ so that we can use that for FDCAN
+    // TODO(blakely): Is this necessary? Can't we just use the PCLK1? CubeMX seems to think so...
+    rcc.pllcfgr
+        .modify(|_, w| w.pllqen().set_bit().pllq().div2());
+    rcc.ccipr.modify(|_, w| w.fdcansel().pllq());
     rcc.apb1enr1.modify(|_, w| w.fdcanen().set_bit());
 
     // Turn on GPIO clocks.
@@ -57,7 +63,7 @@ fn init(
     rcc.apb1enr1.modify(|_, w| w.spi3en().set_bit());
 
     Controller {
-        rcc,
+        fdcan,
         mode_state: Init {
             gpioa,
             gpiob,
@@ -68,8 +74,44 @@ fn init(
 
 impl Controller<Init> {
     pub fn configure_peripherals(mut self) -> Controller<Ready> {
+        let gpioa = self.mode_state.gpioa;
+
+        // Configure GPIO pins
+        // PA11 - FDCAN_RX, PUSHPULL, NOPULL, VERY_HIGH
+        // PA12 - FDCAN_TX, PUSHPULL, NOPULL, VERY_HIGH
+        gpioa.moder.modify(|_, w| {
+            w
+                // FDCAN_RX
+                .moder11()
+                .alternate()
+                // FDCAN_RX
+                .moder12()
+                .alternate()
+        });
+        gpioa.afrh.modify(|_, w| {
+            w
+                // FDCAN_RX
+                .afrh11()
+                .af9()
+                // FDCAN_TX
+                .afrh12()
+                .af9()
+        });
+        gpioa
+            .otyper
+            .modify(|_, w| w.ot11().push_pull().ot12().push_pull());
+        gpioa.ospeedr.modify(|_, w| {
+            w.ospeedr11()
+                .very_high_speed()
+                .ospeedr12()
+                .very_high_speed()
+        });
+        gpioa
+            .pupdr
+            .modify(|_, w| w.pupdr11().floating().pupdr12().floating());
+
         let new_self = Controller {
-            rcc: self.rcc,
+            fdcan: self.fdcan,
             mode_state: Ready {},
         };
         new_self
@@ -82,5 +124,7 @@ pub fn take_hardware() -> Controller<Init> {
     // p.FDCAN1.xidam.write(|w| w.eidm().bits(12));
     // p.TIM1.arr.write(|w| w.bits(123));
 
-    init(cp.NVIC, p.RCC, p.FLASH, p.PWR, p.GPIOA, p.GPIOB, p.GPIOC)
+    init(
+        cp.NVIC, p.RCC, p.FLASH, p.PWR, p.GPIOA, p.GPIOB, p.GPIOC, p.FDCAN1,
+    )
 }
